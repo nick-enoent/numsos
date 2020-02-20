95cfd54dba88d3db48c45dccdf7b497669fbf24d
nick <nick@ovs-5416.ogc.int>
2020-02-20 12:19:39 -0600

    new analysis module metricBinRate - calculate's rate of metric(s) with binned response
    error handling for lustreData module
    remove deprecated compMinMeanMaxRate module

--- graf_analysis/lustreData.py
+++ graf_analysis/lustreData.py
@@ -44,7 +44,7 @@ class lustreData(Analysis):
     def _sum_metrics(self, metrics):
         try:
             ''' Return tuple of (metric_per_second nd array, dataset) '''
-            self.src.select([ 'job_id' ] + metrics,
+            self.src.select([ 'job_id', 'component_id' ] + metrics,
                        from_ = [ self.schema ],
                        where = self.where_,
                        order_by = 'time_job_comp'
@@ -58,13 +58,13 @@ class lustreData(Analysis):
                 if resp is not None:
                     self.xfrm.concat()
             
+            self.xfrm.diff(metrics, group_name='component_id',
+                           keep=['job_id'], xfrm_suffix='')
             sum_ = self.xfrm.max(metrics, group_name='job_id',
                                  keep=['job_id'], xfrm_suffix='')
 
-            sum_ = self.xfrm.sum(metrics, group_name='job_id',
-                                 keep=['job_id'], xfrm_suffix='')
             job_sums = np.zeros(sum_.get_series_size())
-            for m in sum_.series:
+            for m in metrics:
                 job_sums += sum_.array(m)
             return job_sums, sum_
         except Exception as e:
@@ -73,61 +73,66 @@ class lustreData(Analysis):
             return None, None
 
     def get_lustre_avg(self, metrics, threshold, meta=False):
-        sumbytes, sum_ = self._sum_metrics(metrics)
-        if sumbytes is None:
-            return None
-        ret_bps = []
-        ret_jobs = []
-        ret_name = []
-        ret_start = []
-        ret_end = []
-        ret_user = []
-        ret_state = []
-        ret_size = []
-        i = 0
-        sumbytes = np.delete(sumbytes, 0)
-        jids = sum_.array('job_id')[1:]
-        while i < threshold:
-            if len(sumbytes) < 1:
-                break
-            index, val = max(enumerate(sumbytes), key=operator.itemgetter(1))
-            self.src.select(self.job_metrics + ['job_id'],
-                            from_ = [ 'mt-slurm' ],
-                            where = [ [ 'job_id' , Sos.COND_EQ, jids[index] ] ],
-                            order_by = 'job_rank_time'
-                )
-            job = self.src.get_results()
-            if job is None:
+        try:
+            sumbytes, sum_ = self._sum_metrics(metrics)
+            if sumbytes is None:
                 return None
-            if job.array('job_end')[0] < 1:
-                job_end = time.time()
-                ret_end.append(job_end*1000)
-                ret_state.append("In process")
-            else:
-                job_end = job.array('job_end')[0]
-                ret_end.append(job.array('job_end')[0]*1000)
-                ret_state.append("Completed")
-            ret_bps.append(val / (job_end - job.array('job_start')[0]))
-            ret_jobs.append(job.array('job_id')[0])
-            ret_size.append(job.array('job_size')[0])
-            ret_name.append(job.array('job_name')[0])
-            ret_start.append(job.array('job_start')[0] * 1000)
-            ret_user.append(job.array('job_user')[0])
+            ret_bps = []
+            ret_jobs = []
+            ret_name = []
+            ret_start = []
+            ret_end = []
+            ret_user = []
+            ret_state = []
+            ret_size = []
+            i = 0
+            sumbytes = np.delete(sumbytes, 0)
+            jids = sum_.array('job_id')[1:]
+            while i < threshold:
+                if len(sumbytes) < 1:
+                    break
+                index, val = max(enumerate(sumbytes), key=operator.itemgetter(1))
+                self.src.select(self.job_metrics + ['job_id'],
+                                from_ = [ 'mt-slurm' ],
+                                where = [ [ 'job_id' , Sos.COND_EQ, jids[index] ] ],
+                                order_by = 'job_rank_time'
+                    )
+                job = self.src.get_results()
+                if job is None:
+                    return None
+                if job.array('job_end')[0] < 1:
+                    job_end = time.time()
+                    ret_end.append(job_end*1000)
+                    ret_state.append("In process")
+                else:
+                    job_end = job.array('job_end')[0]
+                    ret_end.append(job.array('job_end')[0]*1000)
+                    ret_state.append("Completed")
+                ret_bps.append(val / (job_end - job.array('job_start')[0]))
+                ret_jobs.append(job.array('job_id')[0])
+                ret_size.append(job.array('job_size')[0])
+                ret_name.append(job.array('job_name')[0])
+                ret_start.append(job.array('job_start')[0] * 1000)
+                ret_user.append(job.array('job_user')[0])
 
-            # remove job with highest bps from list of jobs
-            sumbytes = np.delete(sumbytes, index)
-            jids = np.delete(jids, index)
-            i += 1
-        res_ = DataSet()
-        if not meta:
-            res_.append_array(len(ret_bps), 'bps', ret_bps)
-        else:
-            res_.append_array(len(ret_bps), 'ios', ret_bps)
-        res_.append_array(len(ret_jobs), 'job_id', ret_jobs)
-        res_.append_array(len(ret_size), 'ranks', ret_size)
-        res_.append_array(len(ret_name), 'job_name', ret_name)
-        res_.append_array(len(ret_user), 'job_user', ret_user)
-        res_.append_array(len(ret_start), 'job_start', ret_start)
-        res_.append_array(len(ret_end), 'job_end', ret_end)
-        res_.append_array(len(ret_state), 'job_state', ret_state)
-        return res_
+                # remove job with highest bps from list of jobs
+                sumbytes = np.delete(sumbytes, index)
+                jids = np.delete(jids, index)
+                i += 1
+            res_ = DataSet()
+            if not meta:
+                res_.append_array(len(ret_bps), 'bps', ret_bps)
+            else:
+                res_.append_array(len(ret_bps), 'ios', ret_bps)
+            res_.append_array(len(ret_jobs), 'job_id', ret_jobs)
+            res_.append_array(len(ret_size), 'ranks', ret_size)
+            res_.append_array(len(ret_name), 'job_name', ret_name)
+            res_.append_array(len(ret_user), 'job_user', ret_user)
+            res_.append_array(len(ret_start), 'job_start', ret_start)
+            res_.append_array(len(ret_end), 'job_end', ret_end)
+            res_.append_array(len(ret_state), 'job_state', ret_state)
+            return res_
+        except Exception as e:
+            a, b, c = sys.exc_info()
+            print(str(e)+' '+str(c.tb_lineno))
+            return None
---
+++ graf_analysis/metricRateBin.py
@@ -0,0 +1,71 @@
+import os, sys, traceback
+import datetime as dt
+from grafanaAnalysis import Analysis
+from grafanaFormatter import DataSetFormatter
+from numsos.DataSource import SosDataSource
+from numsos.Transform import Transform
+from sosdb.DataSet import DataSet
+from sosdb import Sos
+import pandas as pd
+import numpy as np
+
+class metricRateBin(Analysis):
+    def __init__(self, cont, start, end, schema='Lustre_Client', maxDataPoints=4096):
+        self.schema = schema
+        self.src = SosDataSource()
+        self.src.config(cont=cont)
+        self.start = start
+        self.end = end
+        self.maxDataPoints = maxDataPoints
+
+    def get_data(self, metrics, job_id=0, params='buckets=50'):
+        result = []
+        datapoints = []
+        where_ = [ [ 'timestamp', Sos.COND_GE, self.start ],
+                   [ 'timestamp', Sos.COND_LE, self.end ]
+            ]
+        if job_id > 0:
+            where_.append(['job_id', Sos.COND_EQ, job_id])
+        try:
+            self.src.select(metrics + ['timestamp','component_id' ],
+                       from_ = [ self.schema ],
+                       where = where_,
+                       order_by = 'time_comp_job'
+                )
+            inp = None
+
+            # default for now is dataframe - will update with dataset vs dataframe option
+            self.xfrm = Transform(self.src, None, limit=4096)
+            resp = self.xfrm.begin()
+            if resp is None:
+                print('resp == None')
+                return None
+
+            while resp is not None:
+                resp = self.xfrm.next()
+                if resp is not None:
+                    self.xfrm.concat()
+
+            self.xfrm.diff(metrics, group_name="component_id",
+                           keep=['timestamp'], xfrm_suffix='')
+
+            data = self.xfrm.pop()
+            hsum = None
+            time_range = self.end - self.start
+            bin_width= int(time_range / 50)
+            for met_diff in metrics:
+                os = data.array(met_diff)
+                h = np.histogram(data.array('timestamp').astype('float'), bins=50,
+                                 weights=os, density=False)
+                if hsum is None:
+                    ts = h[1][:-1] / 1000
+                    hsum = np.zeros(h[0].shape)
+                hsum += h[0]
+            res = DataSet()
+            res.append_array(len(hsum), str(metrics)+' '+str(bin_width)+'S bins', hsum)
+            res.append_array(len(ts), 'timestamp', ts) 
+            return res 
+        except Exception as e:
+            a, b, c = sys.exc_info()
+            print(str(e)+' '+str(c.tb_lineno))
+
